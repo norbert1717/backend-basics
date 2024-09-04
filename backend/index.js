46a1e2b19523c3318635fbe0e677610c63e044cb
norbert1717 <miklosnorbert17@gmail.com>
2024-09-04 16:39:12 +0200

    added static directory. and fetch data from backend

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,25 @@ expressnek van egy alap váza, amiből érdemes kiindulni
 npm init -y - mindenből az alapot adja és az alapján rakja össze a json filét
 mindig le kell állítani a honlapot ctrl + c segítségével ahhoz, hogy lássam a változásokat,
 mivel az előző verziója fut a kódnak backendnek
+ENOENT - nem találja az adott helyen az adott filet
+ha html kódban elől van a / jel, akkor mindenhogy működik az oldal
+end pointokat érdemes külön csoportosítani
 */
 
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  res.sendFile(path.join(__dirname, '/../frontend/index.html'))
+  res.send("hello world")
+})
+
+app.use('/public', express.static(path.join(__dirname, '/../frontend/public')))
+
+app.get('/data', (req, res) => {
+  res.json({ name: "kismacska" })
 })
 
 app.listen(port, () => {
